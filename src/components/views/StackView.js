333551bd00ce7a9e9917cfb12c78b496fcb33792
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2020-04-17 12:29:20 +0200

    commit

--- src/components/views/StackView.js
+++ src/components/views/StackView.js
@@ -11,12 +11,16 @@ import reactIcon from 'assets/images/icons8-react-160.png';
 import jqueryIcon from 'assets/images/iconfinder_jquery_308442.png';
 import bootstrapIcon from 'assets/images/icons8-bootstrap-144.png';
 import scIcon from 'assets/images/styled-components.png';
-import rwdIcon from 'assets/images/iconfinder_contact_phone_overlay_5998.png';
 import seoIcon from 'assets/images/iconfinder_Graph-Magnifier_379472.png';
 import npmIcon from 'assets/images/icons8-npm-144.png';
 import yarnIcon from 'assets/images/iconfinder_yarn_4691251.png';
 import sbIcon from 'assets/images/storybook.svg';
 import gitIcon from 'assets/images/icons8-github-150.png';
+
+import { TabletMobileCombo } from '@styled-icons/entypo';
+import { MobileFriendly } from '@styled-icons/material-outlined';
+import { Search } from '@styled-icons/fa-solid';
+
 
 
 const StackViewWrapper = styled.div`
@@ -25,9 +29,10 @@ const StackViewWrapper = styled.div`
   justify-content: flex-start;
   align-items: center;
 
-  h1 {
+  h2 {
     margin: 1%;
     color: #FFE48F;
+    text-transform: uppercase;
   }
 `;
 
@@ -37,6 +42,19 @@ const StackViewIconsContainer = styled.div`
   justify-content: space-evenly;
 
   width: 100%;
+  margin-bottom: 2%;
+
+  > div {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 700;
+
+    p {
+      margin-left: 2%;
+    }
+  }
 `;
 
 const StackViewImage = styled.figure`
@@ -57,75 +75,99 @@ const StackViewImage = styled.figure`
 `;
 
 function StackView() {
-    const mainTechnologies = [
-        { id: 1, src: htmlIcon, alt: "htmlIcon", caption: "HTML5", },
-        { id: 2, src: cssIcon, alt: "cssIcon", caption: "CSS3" },
-        { id: 3, src: sassIcon, alt: "sassIcon", caption: "Sass/SCSS" },
-        { id: 4, src: jsIcon, alt: "jsIcon", caption: "JavaScript" },
-        { id: 5, src: reactIcon, alt: "reactIcon", caption: "ReactJS", },
-    ];
+  const mainTechnologies = [
+    { id: 1, src: htmlIcon, alt: "htmlIcon", caption: "HTML5", },
+    { id: 2, src: cssIcon, alt: "cssIcon", caption: "CSS3" },
+    { id: 3, src: sassIcon, alt: "sassIcon", caption: "Sass/SCSS" },
+    { id: 4, src: jsIcon, alt: "jsIcon", caption: "JavaScript" },
+    { id: 5, src: reactIcon, alt: "reactIcon", caption: "ReactJS", },
+  ];
 
-    const mapMainTechnologies = mainTechnologies.map((el) => (
-      <StackViewImage>
-        <img
-          key={el.id}
-          src={el.src}
-          alt={el.alt}
-          style={{width: "144px"}}
-        />
-        <figcaption>{el.caption}</figcaption>
-      </StackViewImage>
-        )
-    );
+  const mapMainTechnologies = mainTechnologies.map((el) => (
+    <StackViewImage>
+      <img
+        key={el.id}
+        src={el.src}
+        alt={el.alt}
+        style={{ width: "144px" }}
+      />
+      <figcaption>{el.caption}</figcaption>
+    </StackViewImage>
+  )
+  );
 
-    const secondaryTechnologies = [
-        { id: 6, src: jqueryIcon, alt: "jqueryIcon", caption: "jQuery" },
-        { id: 7, src: bootstrapIcon, alt: "bootstrapIcon", caption: "Bootstrap" },
-        { id: 8, src: scIcon, alt: "styledComponentsIcon", caption: "Styled Components" },
-        { id: 9, src: rwdIcon, alt: "rwdIcon", caption: "RWD/Mobile First" },
-        { id: 10, src: seoIcon, alt: "seoIcon", caption: "SEO" },
-        { id: 11, src: npmIcon, alt: "npmIcon", caption: "NPM" },
-        { id: 12, src: yarnIcon, alt: "yarnIcon", caption: "YARN" },
-        { id: 12, src: sbIcon, alt: "sbIcon", caption: "Storybook" },
-        { id: 13, src: gitIcon, alt: "gitIcon", caption: "Git" },
-    ];
+  const additionalTechnologies = [
+    { id: 14, src: TabletMobileCombo, alt: "rwdIcon", size: "15", title: "RWD", text: "Responsive Web Design" },
+    { id: 15, src: MobileFriendly, alt: "mobileFirstIcon", size: "15", title: "Mobile First", text: "Mobile First" },
+    { id: 16, src: Search, alt: "seoIcon", size: "15", title: "SEO", text: "Search Engine Optimization" }
+  ]
 
-    const mapSecondaryTechnologies = secondaryTechnologies.map((el) => (
-      <StackViewImage>
-        <img
-          key={el.id}
-          src={el.src}
-          alt={el.alt}
-                style={{ width: "60px" }}
-        />
-        <figcaption>{el.caption}</figcaption>
-      </StackViewImage>
-      )
-    )
+  const mapAdditionalTechnologies = additionalTechnologies.map((el) => (
+    <div>
+      <{el.src}
+        key={el.id}
+        size={el.size}
+        title={el.title}
+        alt={el.alt}
+      />
+      <p>{el.text}</p>
+    </div>
+  )
+  )
 
-    return (
-      <>
-        <StackViewWrapper>
-          <h1>GŁÓWNE TECHNOLOGIE</h1>
-          <StackViewIconsContainer>
-            {mapMainTechnologies}
-          </StackViewIconsContainer>
-          <h1>PONADTO</h1>
-          <StackViewIconsContainer>
-            {mapSecondaryTechnologies}
-          </StackViewIconsContainer>
-          {/* <span>Atomic Design</span> */}
+  const secondaryTechnologies = [
+    { id: 6, src: jqueryIcon, alt: "jqueryIcon", caption: "jQuery" },
+    { id: 7, src: bootstrapIcon, alt: "bootstrapIcon", caption: "Bootstrap" },
+    { id: 8, src: scIcon, alt: "styledComponentsIcon", caption: "Styled Components" },
+    { id: 10, src: seoIcon, alt: "seoIcon", caption: "SEO" },
+    { id: 11, src: npmIcon, alt: "npmIcon", caption: "NPM" },
+    { id: 12, src: yarnIcon, alt: "yarnIcon", caption: "YARN" },
+    { id: 12, src: sbIcon, alt: "sbIcon", caption: "Storybook" },
+    { id: 13, src: gitIcon, alt: "gitIcon", caption: "Git" },
+  ];
+
+  const mapSecondaryTechnologies = secondaryTechnologies.map((el) => (
+    <StackViewImage>
+      <img
+        key={el.id}
+        src={el.src}
+        alt={el.alt}
+        style={{ width: "60px" }}
+      />
+      <figcaption>{el.caption}</figcaption>
+    </StackViewImage>
+  )
+  )
+
+  return (
+    <>
+      <StackViewWrapper>
+        <h2>koduję głównie w technologiach</h2>
+        <StackViewIconsContainer>
+          {mapMainTechnologies}
+        </StackViewIconsContainer>
+        <h2>
+          zgodnie z zasadami
+        </h2>
+        <StackViewIconsContainer>
+          {mapAdditionalTechnologies}
+        </StackViewIconsContainer>
+        <h2>PONADTO</h2>
+        <StackViewIconsContainer>
+          {mapSecondaryTechnologies}
+        </StackViewIconsContainer>
+        {/* <span>Atomic Design</span> */}
 
 
-          {/* <h1>PROJEKTY KODUJĘ Z:</h1>
+        {/* <h1>PROJEKTY KODUJĘ Z:</h1>
           <span>VSCode</span>
           <span>ESLint</span>
           <span>Prettier</span>
           <span>Linux</span>
           <span>Adobe XD</span>  */}
-        </StackViewWrapper>
-      </>
-    );
+      </StackViewWrapper>
+    </>
+  );
 }
 
 export default StackView;
